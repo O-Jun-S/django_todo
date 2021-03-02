17cdba6be89b5268e000d35d96ca33ee70f56714
O-Jun-S <junsannnn3562@icloud.com>
2021-03-02 20:51:07 +0900

    Changed model for admin.

--- app/mysite/todo/admin.py
+++ app/mysite/todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task
+
 
 # Register your models here.
+#class TaskAdmin(admin.ModelAdmin) {
+#    fieldsets = [
+#        (None, {"fields": ["title"]}),
+#        (None, {"fields": ["text"]})
+#    ]
+#}
+
+
+admin.site.register(Task)
--- app/mysite/todo/models.py
+++ app/mysite/todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=20)
     text = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
