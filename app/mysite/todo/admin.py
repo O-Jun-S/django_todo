069db3d847261be91c639f3fe0107848966b636d
O-Jun-S <junsannnn3562@icloud.com>
2021-03-02 20:55:07 +0900

    Created admin class.

--- app/mysite/todo/admin.py
+++ app/mysite/todo/admin.py
@@ -3,12 +3,8 @@ from .models import Task
 
 
 # Register your models here.
-#class TaskAdmin(admin.ModelAdmin) {
-#    fieldsets = [
-#        (None, {"fields": ["title"]}),
-#        (None, {"fields": ["text"]})
-#    ]
-#}
+class TaskAdmin(admin.ModelAdmin):
+    fields = ["title", "text"]
 
 
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
