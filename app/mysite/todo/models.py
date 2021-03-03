1e3f02cdf81446392d0bf19ab737920aa3e8ad90
O-Jun-S <junsannnn3562@icloud.com>
2021-03-03 17:02:33 +0900

    Changed data name.

--- app/mysite/todo/models.py
+++ app/mysite/todo/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     title = models.CharField(max_length=20)
     text = models.CharField(max_length=50)
     created_date = models.DateTimeField(default=timezone.now)
-    destination_date = models.DateTimeField()
+    limit_date = models.DateTimeField()
 
     def __str__(self):
         return self.title
