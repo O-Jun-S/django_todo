15033f1477a502d638bf2e7fc0ee5da1fa21869a
O-Jun-S <junsannnn3562@icloud.com>
2021-03-03 22:28:26 +0900

    Add create function.

--- app/mysite/todo/urls.py
+++ app/mysite/todo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("index", views.index, name="index"),
-    path("read/<int:task_id>", views.read, name="read")
+    path("read/<int:task_id>", views.read, name="read"),
+    path("create", views.create, name="create"),
+    path("create_task", views.create_task, name="create_task"),
 ]
--- app/mysite/todo/views.py
+++ app/mysite/todo/views.py
@@ -1,12 +1,30 @@
+import datetime
+
 from .models import Task
 from .forms import TaskForm
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, "todo/index.html", {"tasks": Task.objects.all()})
 
+
 def read(request, task_id):
     return render(request, "todo/read.html", {"task": Task.objects.get(id=task_id)})
+
+
+def create(request):
+    return render(request, "todo/create.html", {"form": TaskForm})
+
+
+def create_task(request):
+    title = request.POST["title"]
+    task = Task()
+    task.title = title
+    date_lst = list(map(int, request.POST["limit_date"].split("/")))
+    task.limit_date = datetime.datetime(year=date_lst[0], month=date_lst[1], day=date_lst[2])
+    task.save()
+    return HttpResponseRedirect(reverse('read', args=[task.id]))
