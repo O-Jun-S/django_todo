762fa042ec2498f7bcfe37ce509432283e3509e3
O-Jun-S <junsannnn3562@icloud.com>
2021-03-03 18:04:52 +0900

    Add read page.

--- app/mysite/todo/views.py
+++ app/mysite/todo/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "todo/index.html", {"tasks": Task.objects.all()})
+
+
+def read(request, task_id):
+    return render(request, "todo/read.html", {"task": Task.objects.get(id=task_id)})
